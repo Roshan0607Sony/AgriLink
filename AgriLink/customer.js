1da59602e1d2a259a05b06627e080237ee193286
Roshan0607Sony <roshan0607sony@gmail.com>
2025-10-29 20:24:25 +0530

    Add files via upload

---
+++ AgriLink/customer.js
@@ -0,0 +1,32 @@
+function loadProducts() {
+  const container = document.getElementById("productContainer");
+  const products = JSON.parse(localStorage.getItem("products")) || [];
+  container.innerHTML = "";
+
+  if (products.length === 0) {
+    container.innerHTML = "<p>No products available right now.</p>";
+    return;
+  }
+
+  products.forEach((p, index) => {
+    container.innerHTML += `
+      <div class="product">
+        <h3>${p.name}</h3>
+        <p>${p.desc}</p>
+        <p><b>₹${p.price}</b></p>
+        <button onclick="buyProduct(${index})">Buy Now</button>
+      </div>
+    `;
+  });
+}
+
+function buyProduct(index) {
+  localStorage.setItem("selectedProduct", index);
+  window.location.href = "buy.html";
+}
+
+loadProducts();
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  window.location.href = "start up page.html";
+});
